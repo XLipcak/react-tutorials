e2b0704ebe2a8cc2ced7479a890d25c3ab894d09
jakub.lipcak <jakub.lipcak@garwan.sk>
2018-01-03 12:27:15 +0100

    egghead.io: Getting Started with Redux: Course completed

--- redux-app/src/components/Todo.js
+++ redux-app/src/components/Todo.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Todo = ({ onClick, completed, text }) => (
+const Todo = ({onClick, completed, text}) => (
     <li
         onClick={onClick}
-        style={ {
+        style={{
             textDecoration: completed ? 'line-through' : 'none'
         }}
     >
--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import {render} from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
 import todoApp from './reducers'
 import App from './components/App'
 
@@ -9,7 +9,7 @@ let store = createStore(todoApp)
 
 render(
     <Provider store={store}>
-        <App />
+        <App/>
     </Provider>,
     document.getElementById('root')
 )
